615c9d854ab885d9a472a14e7c3ebdecdb68892a
vortis <xrodrigolima@gmail.com>
2014-09-27 01:53:27 -0300

    Ported for ACF v5
    Made some tweaks on the main acf-code_area.php ( based on
    http://bit.ly/1t3HXV5 ) and used pieces of code from
    acf_code_area-v4.php to create the acf_code_area-v5.php.

--- acf-code_area.php
+++ acf-code_area.php
@@ -10,60 +10,29 @@ License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
+class acf_field_code_area_plugin {
 
-class acf_field_code_area_plugin
-{
-	/*
-	*  Construct
-	*
-	*  @description: 
-	*  @since: 3.6
-	*  @created: 1/04/13
-	*/
-	
-	function __construct()
-	{
-		
-		// version 4+
-		add_action('acf/register_fields', array($this, 'register_fields'));	
+	function __construct() {
+		// version 5
+		add_action('acf/include_field_types', array($this, 'include_field_types'));
 
-		
-		// version 3-
-		add_action( 'init', array( $this, 'init' ), 5);
+		// version 4
+		add_action('acf/register_fields', array($this, 'register_fields'));
+
 	}
-	
-	
-	/*
-	*  Init
-	*
-	*  @description: 
-	*  @since: 3.6
-	*  @created: 1/04/13
-	*/
-	
-	function init()
-	{
-		if(function_exists('register_field'))
-		{ 
-			register_field('acf_field_code_area', dirname(__File__) . '/acf_code_area-v3.php');
-		}
-	}
-	
-	/*
-	*  register_fields
-	*
-	*  @description: 
-	*  @since: 3.6
-	*  @created: 1/04/13
-	*/
-	
-	function register_fields()
-	{
+
+	// version 4
+    function register_fields() {
 		include_once('acf_code_area-v4.php');
 	}
-	
+
+	// version 5
+	function include_field_types( $version ) {
+		include_once('acf_code_area-v5.php');
+	}
+
 }
 
 new acf_field_code_area_plugin();
-		
+
 ?>
