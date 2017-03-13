91dace098c01629acac618b488456e3f806c1e07
Ivan Kalinichenko <kalinichenko@constlab.ru>
2017-03-13 12:56:18 +0300

    fix: fix paths in gulpfile
    bump version to 1.1.2

--- gulpfile.js
+++ gulpfile.js
@@ -7,30 +7,30 @@ var gulp = require('gulp'),
     concat = require('gulp-concat');
 
 gulp.task('css', function () {
-    return gulp.src('../css/codemirror.css')
+    return gulp.src('css/codemirror.css')
         .pipe(csso())
         .pipe(rename(function (path) {
             path.basename += '.min';
         }))
-        .pipe(gulp.dest('../css'));
+        .pipe(gulp.dest('css'));
 });
 
 gulp.task('js', function () {
     return gulp.src([
-        '../js/codemirror.js',
-        '../js/mode/javascript.js',
-        '../js/mode/css.js',
-        '../js/mode/htmlmixed.js',
-        '../js/mode/xml.js',
-        '../js/mode/php.js',
-        '../js/mode/clike.js',
-        '../js/addon/xml-fold.js',
-        '../js/addon/matchtags.js',
-        '../js/acf-code_area.js'
+        'js/codemirror.js',
+        'js/mode/javascript.js',
+        'js/mode/css.js',
+        'js/mode/htmlmixed.js',
+        'js/mode/xml.js',
+        'js/mode/php.js',
+        'js/mode/clike.js',
+        'js/addon/xml-fold.js',
+        'js/addon/matchtags.js',
+        'js/acf-code_area.js'
     ])
         .pipe(uglify())
         .pipe(concat('acf-code_area.min.js'))
-        .pipe(gulp.dest('../js'));
+        .pipe(gulp.dest('js'));
 });
 
 gulp.task('default', ['css', 'js']);
